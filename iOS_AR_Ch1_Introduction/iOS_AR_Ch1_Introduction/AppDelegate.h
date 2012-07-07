9105c07c32db4fda4f3c76717235f371838bcfe6
kedarbhusari <kedarbhusari@gmail.com>
2012-07-06 23:57:02 -0700

    committing first project.

---
+++ iOS_AR_Ch1_Introduction/iOS_AR_Ch1_Introduction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS_AR_Ch1_Introduction
+//
+//  Created by New User on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS_AR_Ch1_Introduction/iOS_AR_Ch1_Introduction/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOS_AR_Ch1_Introduction
+//
+//  Created by New User on 7/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
